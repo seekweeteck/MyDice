b083ed17f0ca90e9375cba02403c45a61047e4f2
seekweeteck <seekt@acd.tarc.edu.my>
2020-01-12 19:47:45 +0800

    Update drawables

--- app/src/main/java/my/edu/tarc/mydice/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/mydice/MainActivity.kt
@@ -2,10 +2,9 @@ package my.edu.tarc.mydice
 
 import android.graphics.drawable.AnimationDrawable
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -14,11 +13,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var imageViewDice : ImageView
     lateinit var dice : AnimationDrawable
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageViewDice = findViewById<ImageView>(R.id.imageViewDice)
+        imageViewDice = findViewById(R.id.imageViewDice)
+
 
         buttonStart.setOnClickListener {
             //Play dice rolling sound
@@ -27,30 +28,55 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer.isLooping = true
 
             //Play dice animation
-            imageViewDice.clearAnimation()
             imageViewDice.setBackgroundResource(R.drawable.dice_animation)
+
             dice = imageViewDice.background as AnimationDrawable
             dice.start()
+
         }
 
         buttonStop.setOnClickListener {
             //Generate a random integer number
             val random = Random.nextInt(1, 6)
-            textViewResult.text = random.toString()
 
             //Stop dice animation
             dice.stop()
 
+            //Stop rolling sound
             mediaPlayer.stop()
 
             when (random) {
-                1 -> imageViewDice.setImageResource(R.drawable.dice1)
-                2 -> imageViewDice.setImageResource(R.drawable.dice2)
-                3 -> imageViewDice.setImageResource(R.drawable.dice3)
-                4 -> imageViewDice.setImageResource(R.drawable.dice4)
-                5 -> imageViewDice.setImageResource(R.drawable.dice5)
+                1 -> imageViewDice.setBackgroundResource(R.drawable.dice1)
+                2 -> imageViewDice.setBackgroundResource(R.drawable.dice2)
+                3 -> imageViewDice.setBackgroundResource(R.drawable.dice3)
+                4 -> imageViewDice.setBackgroundResource(R.drawable.dice4)
+                5 -> imageViewDice.setBackgroundResource(R.drawable.dice5)
                 else ->
-                    imageViewDice.setImageResource(R.drawable.dice6)
+                    imageViewDice.setBackgroundResource(R.drawable.dice6)
+            }
+
+            val area = spinnerArea.selectedItemPosition
+            when(area){
+                0-> {
+                    val result = resources.getStringArray(R.array.love)
+                    textViewResult.text = String.format("%s", result[random].toString())
+                }
+                1->{
+                    val result = resources.getStringArray(R.array.friendship)
+                    textViewResult.text = String.format("%s", result[random].toString())
+                }
+                2->{
+                    val result = resources.getStringArray(R.array.career)
+                    textViewResult.text = String.format("%s", result[random].toString())
+                }
+                3->{
+                    val result = resources.getStringArray(R.array.wellness)
+                    textViewResult.text = String.format("%s", result[random].toString())
+                }
+                else->{
+                    val result = resources.getStringArray(R.array.money)
+                    textViewResult.text = String.format("%s", result[random].toString())
+                }
             }
         }
     }
